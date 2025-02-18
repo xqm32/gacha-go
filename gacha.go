1142799d776d00521ff48602233904bbe541b66d
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-02-19 00:25:47 +0800

    refactor

--- gacha.go
+++ gacha.go
@@ -2,85 +2,123 @@ package gacha
 
 import "math/rand/v2"
 
-type Gacha struct {
-	CharWish  CharWish
-	CharPulls int
-	CharAt    int
-	CherAt    int
-	WeapWish  WeapWish
-	WeapPulls int
-	WeapAt    int
-	WeepAt    int
-
-	OnCharUp   func(g *Gacha)
-	OnChar     func(g *Gacha)
-	OnCharSpec func(g *Gacha)
-	OnCher     func(g *Gacha)
-	OnWeapUp   func(g *Gacha)
-	OnWeap     func(g *Gacha)
-	OnWeapSpec func(g *Gacha)
-	OnWeep     func(g *Gacha)
-}
-
-func (g *Gacha) SetPity(cp, wp int) *Gacha {
-	g.CharWish.Pity, g.WeapWish.Pity = cp, wp
-	return g
-}
-
-func (g *Gacha) PullCharsUp(n int) *Gacha {
-	for n > 0 {
-		g.CharAt = g.CharWish.Pity + 1
-		g.CherAt = g.CharWish.Poty + 1
-		r1, r2 := rand.N(9999)+1, rand.N(9999)+1
-		g.CharWish.Pull(r1, r2)
-		g.CharPulls += 1
-		if g.CharWish.Pity == 0 {
-			if g.CharWish.Guar == 0 {
-				n -= 1
-				if g.CharWish.Spec == 0 && g.OnCharUp != nil {
-					g.OnCharUp(g)
-				} else if g.OnCharSpec != nil {
-					g.OnCharSpec(g)
-				}
-			} else if g.OnChar != nil {
-				g.OnChar(g)
-			}
-		} else if g.CharWish.Poty == 0 && g.OnCher != nil {
-			g.OnCher(g)
-		}
+var (
+	Char5Prob = []int{
+		60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60,
+		60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60,
+		60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60,
+		60, 660, 1260, 1860, 2460, 3060, 3660, 4260, 4860, 5460, 6060, 6660, 7260, 7860, 8460, 9060,
+		9660, 10260,
 	}
-	return g
+	Char4Prob = []int{510, 510, 510, 510, 510, 510, 510, 510, 5610, 10710}
+)
+
+type CharResult int
+
+const (
+	Char5Up CharResult = iota
+	Char5Spec
+	Char5
+	Char4
+	Char3
+)
+
+type CharWish struct {
+	Pity5 int
+	Guar  int
+	Spec  int
+	Pity4 int
 }
 
-func (g *Gacha) PullWeapsUp(n int) *Gacha {
-	for n > 0 {
-		g.WeapAt = g.WeapWish.Pity + 1
-		g.WeepAt = g.WeapWish.Poty + 1
-		r1, r2 := rand.N(9999)+1, rand.N(9999)+1
-		g.WeapWish.Pull(r1, r2)
-		g.WeapPulls += 1
-		if g.WeapWish.Pity == 0 {
-			if g.WeapWish.Guar == 0 {
-				n -= 1
-				if g.OnWeapUp != nil {
-					g.OnWeapUp(g)
-				}
-			} else if g.WeapWish.Spec == 0 && g.OnWeapSpec != nil {
-				g.OnWeapSpec(g)
-			} else if g.OnWeap != nil {
-				g.OnWeap(g)
-			}
-		} else if g.WeapWish.Poty == 0 && g.OnWeep != nil {
-			g.OnWeep(g)
-		}
+func (w *CharWish) Pull() CharResult {
+	return w.PullR(rand.N(9999)+1, rand.N(9999)+1)
+}
+
+func (w *CharWish) PullN(n int) []CharResult {
+	res := make([]CharResult, n)
+	for i := 0; i < n; i++ {
+		res[i] = w.Pull()
 	}
-	return g
+	return res
 }
 
-func (g *Gacha) PullUp(cn, wn int) *Gacha {
-	return g.PullCharsUp(cn).PullWeapsUp(wn)
+func (w *CharWish) PullR(r1, r2 int) CharResult {
+	if r1 <= Char5Prob[w.Pity5] {
+		if (w.Guar == 0 && 1 <= r2 && r2 <= 5000) || (w.Guar == 1) {
+			w.Pity5, w.Guar, w.Spec, w.Pity4 = 0, 0, 0, w.Pity4+1
+			return Char5Up
+		} else if w.Guar == 0 && 5001 <= r2 && r2 <= 5500 {
+			w.Pity5, w.Guar, w.Spec, w.Pity4 = 0, 0, 1, w.Pity4+1
+			return Char5Spec
+		} else {
+			w.Pity5, w.Guar, w.Spec, w.Pity4 = 0, 1, 0, w.Pity4+1
+			return Char5
+		}
+	} else if w.Pity4 >= 10 || r2 <= Char4Prob[w.Pity4] {
+		w.Pity5, w.Spec, w.Pity4 = w.Pity5+1, 0, 0
+		return Char4
+	} else {
+		w.Pity5, w.Spec, w.Pity4 = w.Pity5+1, 0, w.Pity4+1
+		return Char3
+	}
 }
 
-func (g *Gacha) Pulls() int {
-	return g.CharPulls + g.WeapPulls
+var (
+	Weap5Prob = []int{
+		70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+		70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+		70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 770, 1470, 2170, 2870, 3570, 4270,
+		4970, 5670, 6370, 7070, 7770, 8470, 9170, 9870, 10570, 11270, 11970, 12670,
+	}
+	Weap4Prob = []int{600, 600, 600, 600, 600, 600, 600, 6600, 12600, 18600}
+)
+
+type WeapResult int
+
+const (
+	Weap5Up WeapResult = iota
+	Weap5Spec
+	Weap5
+	Weap4
+	Weap3
+)
+
+type WeapWish struct {
+	Pity5 int
+	Guar  int
+	Spec  int
+	Pity4 int
+}
+
+func (w *WeapWish) Pull() WeapResult {
+	return w.PullR(rand.N(9999)+1, rand.N(9999)+1)
+}
+
+func (w *WeapWish) PullN(n int) []WeapResult {
+	res := make([]WeapResult, n)
+	for i := 0; i < n; i++ {
+		res[i] = w.Pull()
+	}
+	return res
+}
+
+func (w *WeapWish) PullR(r1, r2 int) WeapResult {
+	if r1 <= Weap5Prob[w.Pity5] {
+		if (w.Guar == 0 && 1 <= r2 && r2 <= 3750) || (w.Guar == 1) {
+			w.Pity5, w.Guar, w.Spec, w.Pity4 = 0, 0, 0, w.Pity4+1
+			return Weap5Up
+		} else if w.Guar == 0 && 3751 <= r2 && r2 <= 7500 {
+			w.Pity5, w.Guar, w.Spec, w.Pity4 = 0, 1, 1, w.Pity4+1
+			return Weap5Spec
+		} else {
+			w.Pity5, w.Guar, w.Spec, w.Pity4 = 0, 1, 0, w.Pity4+1
+			return Weap5
+		}
+	} else if w.Pity4 >= 10 || r2 <= Weap4Prob[w.Pity4] {
+		w.Pity5, w.Spec, w.Pity4 = w.Pity5+1, 0, 0
+		return Weap4
+	} else {
+		w.Pity5, w.Spec, w.Pity4 = w.Pity5+1, 0, w.Pity4+1
+		return Weap3
+	}
 }
